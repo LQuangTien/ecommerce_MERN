8d420af37d1517389fe1cf644ceca11300577b77
LQuangTien <tienkg5554@gmail.com>
2021-07-01 11:35:56 +0700

    fix gitignore

--- server/index.js
+++ server/index.js
@@ -23,5 +23,5 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api", authRoutes);
 app.use("/api", adminRoutes);
 app.listen(process.env.PORT, () => {
-  console.log("Server is running on port: ", process.env.PORT);
+  console.log("Server is running on port:", process.env.PORT);
 });
