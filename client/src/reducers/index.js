63941f1848d66bd5eafa45b9afda9b141e0e62bd
LQuangTien <tienkg5554@gmail.com>
2021-07-22 22:05:01 +0700

    Client: Sign in

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
+import authReducer from "./auth.reducers";
 import categoryReducer from "./category.reducers";
 import productReducer from "./product.reducers";
 const rootReducer = combineReducers({
   categories: categoryReducer,
   products: productReducer,
+  auth: authReducer
 });
 
 export default rootReducer;
