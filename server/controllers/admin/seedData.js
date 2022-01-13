3ede7ef1edc45ff4a0a1888dac4051a38bbd9a75
VoHoangAn <vohoangan2000@gmail.com>
2022-01-13 19:45:26 +0700

    fix seed data file

--- server/controllers/admin/seedData.js
+++ server/controllers/admin/seedData.js
@@ -34,28 +34,39 @@ exports.seedDataProduct = async (req, res) => {
             });
     });
 
+    //C:\Users\USER\Desktop\test\node_modules\faker\lib\locales\en\music\genre.js
+    // try {
+    //     const findProduct = await Product.find({},'_id salePrice');
+    //     console.log(findProduct)
+    //     fs.writeFile(`C:/Users/USER/Desktop/test/node_modules/faker/lib/locales/en/music/genre.js`,JSON.stringify(findProduct) , function (err, data) {
+    //         if (err) console.log(err);
+    //     });
+    // } catch (error) {
+    //     console.log(error);
+    // }
+   
     // Product.deleteMany({category:"PC"}, function (err) { console.log(err) })
 
-    return Get(res,{stop:1})
+    return Get(res,{stop:1});
 }
 
 exports.seedDataOrder = (req, res) => {
     const orderData = path.join(__dirname, '..', '..', 'data', 'order.json');
 
-    // fs.readFile(`C:/Users/USER/Desktop/test/bigDataSet.json`, 'utf8', function (err, data) {
-    //     if (err) throw err;
-    //     obj = JSON.parse(data);
+    fs.readFile(`C:/Users/USER/Desktop/test/bigDataSet.json`, 'utf8', function (err, data) {
+        if (err) throw err;
+        obj = JSON.parse(data);
 
-    //     Order.insertMany(obj)
-    //         .then(function (mongooseDocuments) {
-    //             console.log(mongooseDocuments)
-    //         })
-    //         .catch(function (err) {
-    //             console.log(err)
-    //         });
-    // });
+        Order.insertMany(obj)
+            .then(function (mongooseDocuments) {
+                console.log(mongooseDocuments)
+            })
+            .catch(function (err) {
+                console.log(err)
+            });
+    });
 
     // Order.deleteMany({ }, function (err) { console.log(err) })
-    Order.updateMany({addressId:'60ffc3bdb60e13260c448b14' }, function (err) { console.log(err) })
+    // Order.updateMany({addressId:'60ffc3bdb60e13260c448b14' }, function (err) { console.log(err) })
     return Get(res,{stop:1})
 }
